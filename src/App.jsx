2de16be423d1f0a2b070f0c1dc259ff3b0615d16
daramallapavan <daramallapavan123@gmail.com>
2024-12-28 18:36:05 +0530

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,29 @@
-import { Outlet } from 'react-router-dom'
+import { Outlet, useNavigate } from 'react-router-dom'
 import './App.css'
+import { useState } from 'react'
 
 function App() {
 
+const navigate=useNavigate()
 
+const [visible,setVisible]=useState(true)
+  const openHome=()=>{
+    setVisible(false)
+    navigate('/home')
+
+  }
 
   return (
     <>
  
   <div>
+  {
+    visible && (
+      <div className='flex items-center justify-center py-6'>
+      <p className='px-4 py-1 border shadow' onClick={openHome}>Click here</p>
+    </div>
+    )
+  }
   <Outlet/>
   </div>
   
